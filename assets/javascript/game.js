230a7e2da9dc612757db11cbf8dba06df7a58b20
Syed T. Akbar <sakbar01@icloud.com>
2019-08-03 22:16:10 -0500

    completed development

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,70 @@
 
-    const NUM_OF_GUESSES = 12;
  
     let crystalGame = {
+
         pointScore: 0,
         numOfWins: 0,
         numOfLosses: 0,
         randValue: 0,
 
+        minCrystalVal: 1,
+        maxCrystalVal: 12,
+
+        minRandVal: 19,
+        maxRandVal: 120,
+
+        defaultScore: 0,
+
+
         getRandomInt: function (min, max) {
-            min = Math.ceil(min);
-            max = Math.floor(max);
+             min = Math.ceil(min);
+             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min + 1)) + min;
         },
 
-        initialize: function() {
+        resetGame: function() {
 
-            randValue = crystalGame.getRandomInt (30, 80);
-            document.getElementById("rand-num-val").innerHTML = randValue;
+            this.pointScore = this.defaultScore;
 
-            document.querySelectorAll(".crystal-image").forEach(function(node){            
-                node.setAttribute("rand-val", crystalGame.getRandomInt (13, 50));
-                // console.log("inside load " + node.getAttribute("rand-val"));
+            this.randValue = this.getRandomInt (this.minRandVal, this.maxRandVal);
+            document.getElementById("rand-num-val").innerHTML = this.randValue;
+            document.getElementById("player-score").innerText  = this.defaultScore;
+
+            let rootObj = this;
+    
+            document.querySelectorAll(".crystal-image").forEach(function(node){     
+                const crystalRanVal =  rootObj.getRandomInt (rootObj.minCrystalVal, rootObj.maxCrystalVal);                                   
+                node.setAttribute("rand-val", crystalRanVal);                
             })
         },  
 
         calcScore: function() {
 
+            let isWinOrLoos = false;
+
             if (parseInt(this.pointScore) === parseInt(this.randValue))
             {
-                this.numOfWins++;
+                this.numOfWins++;     
+                isWinOrLoos = true;          
             }
-            else
+            else if (parseInt(this.pointScore) > parseInt(this.randValue))
             {
-                this.numOfLosses++;
+                this.numOfLosses++;      
+                isWinOrLoos = true;                        
+            }
+
+            if (isWinOrLoos)
+            {
+                document.getElementById("num-of-wins").innerHTML = "Wins: " + this.numOfWins;
+                document.getElementById("num-of-losses").innerHTML = "Losses: " + this.numOfLosses;
+    
+                this.resetGame();
             }
         },
-
-        displayScore: function () {
-            document.getElementById("num-of-wins").innerHTML = "Wins: " + this.numOfWins;
-            document.getElementById("num-of-losses").innerHTML = "Losses: " + this.numOfLosses;
-        }
     };
 
     window.onload = function(){
-        crystalGame.initialize();   
+        crystalGame.resetGame();   
      
     }
 
@@ -53,11 +74,9 @@
             crystalGame.pointScore += parseInt(node.getAttribute("rand-val"));
             document.getElementById("player-score").innerText  = crystalGame.pointScore;
 
-            crystalGame.calcScore();
-            crystalGame.displayScore();
-
+            crystalGame.calcScore(); 
             
         });
-      })
+    })
 
 
